b3e08f286a79aacac2880f281d6af66e36c62c87
Datasound9087 <samlharper133@gmail.com>
2022-02-22 16:02:54 +0000

    Add profile summary info to main menu

--- Assets/Scripts/UI/MainMenu/ProfilePanel.cs
+++ Assets/Scripts/UI/MainMenu/ProfilePanel.cs
@@ -7,12 +7,24 @@ public class ProfilePanel : MonoBehaviour
 {
     public GameObject profilesSelectionPanel;
     public GameObject profile1Button;
+    public GameObject profile1LevelsHighScoreText;
+    public GameObject profile1EndlessHighScoreText;
     public GameObject profile2Button;
+    public GameObject profile2LevelsHighScoreText;
+    public GameObject profile2EndlessHighScoreText;
     public GameObject profile3Button;
+    public GameObject profile3LevelsHighScoreText;
+    public GameObject profile3EndlessHighScoreText;
     public GameObject profile4Button;
+    public GameObject profile4LevelsHighScoreText;
+    public GameObject profile4EndlessHighScoreText;
 
     private MainMenu mainMenu;
     private ProfileCreationPanel profileCreationPanel;
+
+    private const string LevelsHighScoreText = "Levels: ";
+    private const string EndlessHighScoreText = "Endless: ";
+    private const int MaxNumberOfProfiles = 4;
 
     void Awake()
     {
@@ -61,25 +73,39 @@ public class ProfilePanel : MonoBehaviour
             return;
         }
 
-        if (profiles.Count >= 1)
+        // Trick for populating profiles without loads of duplicated code
+        // make possible to reference related gameobjects via an index, then load all at once
+        GameObject[] profileButtons = new GameObject[] {
+            profile1Button, profile2Button,
+            profile3Button, profile4Button
+        };
+
+        GameObject[] profileLevelsText = new GameObject[] {
+            profile1LevelsHighScoreText, profile2LevelsHighScoreText,
+            profile3LevelsHighScoreText, profile4LevelsHighScoreText
+        };
+
+        GameObject[] profileEndlessText = new GameObject[] {
+            profile1EndlessHighScoreText, profile2EndlessHighScoreText,
+            profile3EndlessHighScoreText, profile4EndlessHighScoreText
+        };
+
+        for (int i = 0; i < MaxNumberOfProfiles; i++)
         {
-            profile1Button.GetComponentInChildren<TextMeshProUGUI>().SetText(profiles[0].name);
+            if (i == profiles.Count)
+            {
+                break;
+            }
+            Profile profile = profiles[i];
+            profileButtons[i].GetComponentInChildren<TextMeshProUGUI>().SetText(profile.name);
+            PopulateScoresForProfile(profile, profileLevelsText[i], profileEndlessText[i]);
         }
-        
-        if (profiles.Count >= 2)
-        {
-            profile2Button.GetComponentInChildren<TextMeshProUGUI>().SetText(profiles[1].name);
-        } 
-        
-        if (profiles.Count >= 3)
-        {
-            profile3Button.GetComponentInChildren<TextMeshProUGUI>().SetText(profiles[2].name);
-        } 
-        
-        if (profiles.Count >= 4)
-        {
-            profile4Button.GetComponentInChildren<TextMeshProUGUI>().SetText(profiles[3].name);
-        }
+    }
+
+    private void PopulateScoresForProfile(Profile profile, GameObject levelsText, GameObject endlessText)
+    {
+        levelsText.GetComponent<TextMeshProUGUI>().SetText(LevelsHighScoreText + profile.levelsHighScore);
+        endlessText.GetComponent<TextMeshProUGUI>().SetText(EndlessHighScoreText + profile.endlessHighScore);
     }
 
     private void ReturnToMainMenu()
